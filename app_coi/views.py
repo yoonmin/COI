d6a60fd8b11c6f4b79bc7c78152ae79f81567c3e
yoongeemin <ym2363@columbia.edu>
2014-12-21 12:05:55 -0500

    test

--- app_coi/views.py
+++ app_coi/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
-class IndexView(ListView):
+class IndexView(TemplateView):
 	template_name = 'app_coi/index.html'
