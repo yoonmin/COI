65b83b73c210006a9a6bb9164833763b1d4777b3
yoongeemin <ym2363@columbia.edu>
2015-03-20 16:02:54 -0400

    commit

--- COI/static/js/lightSlider/js/jquery.lightSlider.js
+++ COI/static/js/lightSlider/js/jquery.lightSlider.js
@@ -236,7 +236,7 @@
                     var w = parseInt($('.lSSlideWrapper').css('width').replace('px', ''));
                     $('.lSSlideWrapper').css('height', w*0.51367 + 'px');
                     $('.lSSlideWrapper img').css('height', w*0.51367 + 'px');
-                    $('.lslide').css('height', w*0.51367 + 'px');
+                    $('.slider_content').css('height', w*0.51367 + 'px');
                     
 
                     var h = $('.lSSlideWrapper').css('height');
@@ -870,7 +870,7 @@
                 var w = parseInt($('.lSSlideWrapper').css('width').replace('px', ''));
                 $('.lSSlideWrapper').css('height', w*0.51367 + 'px');
                 $('.lSSlideWrapper img').css('height', w*0.51367 + 'px');
-                $('.lslide').css('height', w*0.51367 + 'px');
+                $('.slider_content').css('height', w*0.51367 + 'px');
                 
 
                 var h = $('.lSSlideWrapper').css('height');
@@ -1072,7 +1072,7 @@
                 var w = parseInt($('.lSSlideWrapper').css('width').replace('px', ''));
 
                 $('.lSSlideWrapper').css('height', w*0.51367 + 'px');
-                $('.lSSlideWrapper .lslide').css('height', w*0.51367 + 'px');
+                $('.lSSlideWrapper .slider_content').css('height', w*0.51367 + 'px');
                 $('.lSSlideWrapper img').css('height', w*0.51367 + 'px');
 
                 var h = $('.lSSlideWrapper').css('height');
