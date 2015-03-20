b3b2a514ba381e421f8b0c881aef2f24fb7a5e22
yoongeemin <ym2363@columbia.edu>
2015-03-20 15:20:09 -0400

    commit

--- COI/static/js/lightSlider/js/jquery.lightSlider.js
+++ COI/static/js/lightSlider/js/jquery.lightSlider.js
@@ -232,7 +232,7 @@
                     elSize = settings.verticalHeight;
                     // $slide.css('height', elSize + 'px');
                     $slide.css('width', '85%');
-                    var w = $slide.css('width');
+                    var w = parseInt($('.lSSlideWrapper').css('width').replace('px', ''));
                     $('.lSSlideWrapper').css('height', w*0.51367 + 'px');
                     $('.lSSlideWrapper .lslide').css('height', w*0.51367 + 'px');
                     $('.lSSlideWrapper img').css('height', w*0.51367 + 'px');
